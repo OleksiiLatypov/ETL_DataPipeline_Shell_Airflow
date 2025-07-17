d1578c8469441ffb93224fea8c76b379daf9d962
latypov <105818016+OleksiiLatypov@users.noreply.github.com>
2025-07-17 09:59:28 +0000

    final version

--- assignemt/airflow/dags/dag.py
+++ assignemt/airflow/dags/dag.py
@@ -1,16 +1,8 @@
-#sudo mkdir -p /home/project/airflow/dags/finalassignment/staging
-#sudo chmod -R 777 /home/project/airflow/dags/finalassignment
-#sudo curl https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBM-DB0250EN-SkillsNetwork/labs/Final%20Assignment/tolldata.tgz -o /home/project/airflow/dags/finalassignment/tolldata.tgz
-
-
-# import the libraries
 from datetime import timedelta
 from airflow.models import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.utils.dates import days_ago
 
-
-#defining DAG arguments
 default_args = {
     'owner': 'airflow',
     'start_date': days_ago(0),
@@ -21,9 +13,6 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-
-
-# defining the DAG
 dag = DAG(
     'ETL_toll_data',
     schedule_interval=timedelta(days=1),
@@ -31,59 +20,79 @@ dag = DAG(
     description='Airflow Final Assignment',
 )
 
-#define task unzip_data BashOperator
+
+
+# Create a task named unzip_data to unzip data
 unzip_data = BashOperator(
-    task_id = 'unzip_data',
-    bash_command = 'tar -xzvf /home/project/airflow/dags/finalassignment/tolldata.tgz -C /home/project/airflow/dags/finalassignment',
-    dag=dag
-
+    task_id='unzip_data',
+    bash_command='tar -xzf /home/project/airflow/dags/finalassignment/tolldata.tgz '
+                 '-C /home/project/airflow/dags/finalassignment',
+    dag=dag,
 )
 
-#define task extract_data_from_csv
 
-extract_data_from_csv=BashOperator(
-    task_id = 'extract_data_from_csv',
-    bash_command = 'cut -d "," -f1-4 /home/project/airflow/dags/finalassignment/vehicle-data.csv > /home/project/airflow/dags/finalassignment/csv_data.csv',
-    dag=dag
+# Create a task named extract_data_from_csv to extract the fields Rowid,\
+# Timestamp, Anonymized Vehicle number, and Vehicle type from the vehicle-data.csv\
+# file and save them into a file named csv_data.csv
+
+extract_data_from_csv = BashOperator(
+    task_id='extract_data_from_csv',
+    bash_command='cut -d "," -f1-4 /home/project/airflow/dags/finalassignment/vehicle-data.csv '
+                 '> /home/project/airflow/dags/finalassignment/csv_data.csv',
+    dag=dag,
 )
 
-# define task extract_data_from_tsv
 
+# Create a task named extract_data_from_tsv to extract the fields Number of axles, Tollplaza id,\
+# and Tollplaza code from the tollplaza-data.tsv file and save it into a file named tsv_data.csv
+
+# gsub(REGEX, REPLACEMENT, TARGET)
 extract_data_from_tsv = BashOperator(
-    task_id = 'extract_data_from_tsv',
-    bash_command = 'cut -f5-7 < /home/project/airflow/dags/finalassignment/tollplaza-data.tsv > /home/project/airflow/dags/finalassignment/tsv_data.csv',
-    dag=dag
+    task_id='extract_data_from_tsv',
+    bash_command="cut -f5-7 /home/project/airflow/dags/finalassignment/tollplaza-data.tsv | "
+                 "awk '{gsub(/\\r/, \"\"); print}' | tr '\\t' ',' "
+                 "> /home/project/airflow/dags/finalassignment/tsv_data.csv",
+    dag=dag,
 )
 
 
-# define task extract_data_from_fixed_width
+# Create a task named extract_data_from_fixed_width to extract the fields Type of Payment code, and Vehicle\
+# Code from the fixed width file payment-data.txt and save it into a file named fixed_width_data.csv
 
 extract_data_from_fixed_width = BashOperator(
-    task_id = 'extract_data_from_fixed_width',
-    bash_command = 'cut -c 59- < /home/project/airflow/dags/finalassignment/payment-data.txt > /home/project/airflow/dags/finalassignment/fixed_width_data.csv',
-    dag=dag
+    task_id='extract_data_from_fixed_width',
+    bash_command="cut -c 59- /home/project/airflow/dags/finalassignment/payment-data.txt | "
+                 "awk '{$1=$1; gsub(/[ ]+/, \",\"); print}' "
+                 "> /home/project/airflow/dags/finalassignment/fixed_width_data.csv",
+    dag=dag,
 )
 
 
-# define task consolidate_data
+# Create a task named consolidate_data to consolidate data extracted from previous tasks.\
+# This task should create a single csv file named extracted_data.csv by combining data from the following files:
 
 consolidate_data = BashOperator(
-    task_id= 'consolidate_data',
-    bash_command='paste -d "," /home/project/airflow/dags/finalassignment/csv_data.csv \
-    /home/project/airflow/dags/finalassignment/tsv_data.csv \
-    /home/project/airflow/dags/finalassignment/fixed_width_data.csv \
-    > /home/project/airflow/dags/finalassignment/extracted_data.csv',
-    dag= dag,
+    task_id='consolidate_data',
+    bash_command='paste -d "," '
+                 '/home/project/airflow/dags/finalassignment/csv_data.csv '
+                 '/home/project/airflow/dags/finalassignment/tsv_data.csv '
+                 '/home/project/airflow/dags/finalassignment/fixed_width_data.csv '
+                 '> /home/project/airflow/dags/finalassignment/extracted_data.csv',
+    dag=dag,
 )
 
-# define task transform_data
+
+#Create a task named transform_data to transform the vehicle_type field in extracted_data.csv\
+# into capital letters and save it into a file named transformed_data.csv in the staging directory.
 
 transform_data = BashOperator(
-    task_id= 'transform_data',
-    bash_command= 'tr "[a-z]" "[A-Z]" < /home/project/airflow/dags/finalassignment/extracted_data.csv',
-    dag= dag,
+    task_id='transform_data',
+    bash_command='sed \'s/[^,]*/\\U&/4\' '
+                 '/home/project/airflow/dags/finalassignment/extracted_data.csv '
+                 '> /home/project/airflow/dags/finalassignment/staging/transformed_data.csv',
+    dag=dag,
 )
 
 
-
+# Defining task dependencies
 unzip_data >> extract_data_from_csv >> extract_data_from_tsv >> extract_data_from_fixed_width >> consolidate_data >> transform_data
